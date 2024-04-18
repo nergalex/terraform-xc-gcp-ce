cf2a85c7b1e14b3137644c622a787736691d5b73
c.klewar <c.klewar@f5.com>
2024-04-18 13:34:47 +0200

    add gcp module v15

--- examples/single_node_multi_nic_existing_vpc_and_subnet_nat_no_eip/main.tf
+++ examples/single_node_multi_nic_existing_vpc_and_subnet_nat_no_eip/main.tf
@@ -40,27 +40,6 @@ module "vpc_sli" {
   delete_default_internet_gateway_routes = true
 }
 
-resource "google_compute_address" "nat" {
-  count   = 1
-  name    = "${module.vpc_slo.network_name}-${var.gcp_region}-nat-${count.index}"
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
-
-module "nat" {
-  source                             = "terraform-google-modules/cloud-nat/google"
-  version                            = "~> 2.0"
-  project_id                         = var.gcp_project_id
-  region                             = var.gcp_region
-  router                             = "${var.project_prefix}-${var.f5xc_cluster_name}-nat-router-${var.gcp_region}-${var.project_suffix}"
-  create_router                      = true
-  name                               = "${var.project_prefix}-${var.f5xc_cluster_name}-nat-config-${var.gcp_region}-${var.project_suffix}"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  # nat_ip_allocate_option             = "MANUAL_ONLY"
-  nat_ips                            = google_compute_address.nat.*.self_link
-  network                            = module.vpc_slo.network_name
-}
-
 module "f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_nat_no_eip" {
   depends_on                      = [module.vpc_sli, module.vpc_slo]
   source                          = "../../modules/f5xc/ce/gcp"
@@ -70,11 +49,14 @@ module "f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_nat_no_e
   has_public_ip                   = false
   ssh_public_key                  = file(var.ssh_public_key_file)
   status_check_type               = "cert"
+  gcp_project_id                  = var.gcp_project_id
   gcp_instance_type               = var.gcp_instance_type
   gcp_instance_image              = var.gcp_instance_image
   gcp_instance_disk_size          = var.gcp_instance_disk_size
+  private_ce_gcp_nat_name         = "${var.project_prefix}-${var.f5xc_cluster_name}-nat-config-${var.gcp_region}-${var.project_suffix}"
   gcp_existing_network_slo        = module.vpc_slo.network_name #var.gcp_existing_network_slo
   gcp_existing_network_sli        = module.vpc_sli.network_name #var.gcp_existing_network_sli
+  private_ce_gcp_nat_router_name  = "${var.project_prefix}-${var.f5xc_cluster_name}-nat-router-${var.gcp_region}-${var.project_suffix}"
   gcp_existing_subnet_network_slo = module.vpc_slo.subnets["${var.gcp_region}/${var.project_prefix}-${var.f5xc_cluster_name}-slo-${var.gcp_region}-${var.project_suffix}"]["name"]
   gcp_existing_subnet_network_sli = module.vpc_sli.subnets["${var.gcp_region}/${var.project_prefix}-${var.f5xc_cluster_name}-sli-${var.gcp_region}-${var.project_suffix}"]["name"]
   f5xc_tenant                     = var.f5xc_tenant
@@ -85,6 +67,7 @@ module "f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_nat_no_e
   f5xc_cluster_name               = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
   f5xc_api_p12_file               = var.f5xc_api_p12_file
   f5xc_ce_gateway_type            = var.f5xc_ce_gateway_type
+  f5xc_is_private_cloud_ce        = true
   f5xc_api_p12_cert_password      = var.f5xc_api_p12_cert_password
   f5xc_ce_nodes = {
     node0 = {
