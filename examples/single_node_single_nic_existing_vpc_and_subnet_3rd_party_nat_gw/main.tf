3a6cd003ee7dd395462c341116cae0c438a0eedd
c.klewar <c.klewar@f5.com>
2024-04-22 14:14:41 +0200

    add gcp appstack v19

---
+++ examples/single_node_single_nic_existing_vpc_and_subnet_3rd_party_nat_gw/main.tf
@@ -0,0 +1,46 @@
+locals {
+  custom_tags = {
+    Owner         = var.owner
+    f5xc-tenant   = var.f5xc_tenant
+    f5xc-template = "f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_slo_no_eip_3rd_party_nat_gw"
+  }
+}
+
+module "f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_slo_no_eip_3rd_party_nat_gw" {
+  source                          = "../../modules/f5xc/ce/gcp"
+  owner                           = var.owner
+  is_sensitive                    = false
+  has_public_ip                   = false
+  ssh_public_key                  = file(var.ssh_public_key_file)
+  status_check_type               = "cert"
+  gcp_region                      = var.gcp_region
+  gcp_project_id                  = var.gcp_project_id
+  gcp_instance_type               = var.gcp_instance_type
+  gcp_instance_image              = var.gcp_instance_image
+  gcp_instance_disk_size          = var.gcp_instance_disk_size
+  gcp_existing_network_slo        = var.gcp_existing_network_slo
+  gcp_existing_subnet_network_slo = var.gcp_existing_subnet_network_slo
+  f5xc_tenant                     = var.f5xc_tenant
+  f5xc_api_url                    = var.f5xc_api_url
+  f5xc_namespace                  = var.f5xc_namespace
+  f5xc_api_token                  = var.f5xc_api_token
+  f5xc_token_name                 = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
+  f5xc_cluster_name               = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
+  f5xc_api_p12_file               = var.f5xc_api_p12_file
+  f5xc_ce_gateway_type            = var.f5xc_ce_gateway_type
+  f5xc_api_p12_cert_password      = var.f5xc_api_p12_cert_password
+  f5xc_ce_nodes = {
+    node0 = {
+      az = format("%s-%s", var.gcp_region, var.gcp_zone)
+    }
+  }
+  providers = {
+    google   = google.default
+    volterra = volterra.default
+  }
+}
+
+output "f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_slo_no_eip_3rd_party_nat_gw" {
+  value = module.f5xc_gcp_cloud_ce_single_node_multi_nic_existing_vpc_and_subnet_slo_no_eip_3rd_party_nat_gw
+}
+
---
+++ examples/single_node_single_nic_existing_vpc_and_subnet_3rd_party_nat_gw/variables.tf
@@ -0,0 +1,127 @@
+variable "project_prefix" {
+  type        = string
+  description = "prefix string put in front of string"
+}
+
+variable "project_suffix" {
+  type        = string
+  description = "prefix string put at the end of string"
+}
+
+variable "f5xc_api_p12_file" {
+  description = "F5 XC API certificate file"
+  type        = string
+}
+
+variable "f5xc_api_p12_cert_password" {
+  description = "F5 XC API certificate file password"
+  type        = string
+  default     = ""
+}
+
+variable "f5xc_api_url" {
+  description = "F5 XC API URL"
+  type        = string
+}
+
+variable "f5xc_api_token" {
+  description = "F5 XC API token"
+  type        = string
+}
+
+variable "f5xc_tenant" {
+  description = "F5 XC Tenant name"
+  type        = string
+}
+
+variable "f5xc_namespace" {
+  description = "F5 XC namespace name"
+  type        = string
+  default     = "system"
+}
+
+variable "f5xc_cluster_name" {
+  description = "F5 XC CE cluster name"
+  type        = string
+}
+
+variable "owner" {
+  description = "GCP tag owner email address"
+  type        = string
+}
+
+variable "f5xc_ce_gateway_type" {
+  description = "F5 XC CE gateway type"
+  type        = string
+  default     = "ingress_gateway"
+}
+
+variable "f5xc_cluster_latitude" {
+  description = "F5 XC CE geo latitude"
+  type        = number
+  default     = -73.935242
+}
+
+variable "f5xc_cluster_longitude" {
+  description = "F5 XC CE geo longitude"
+  type        = number
+  default     = 40.730610
+}
+
+variable "ssh_public_key_file" {
+  description = "path to ssh public key file"
+  type        = string
+}
+
+variable "gcp_project_id" {
+  description = "GCP account project id"
+  type        = string
+}
+
+variable "gcp_application_credentials" {
+  description = "GCP account credentials"
+  type        = string
+  default     = ""
+}
+
+variable "gcp_instance_type" {
+  type    = string
+  default = "n1-standard-4"
+}
+
+variable "gcp_instance_disk_size" {
+  type    = string
+  default = "40"
+}
+
+variable "gcp_region" {
+  type = string
+}
+
+variable "gcp_zone" {
+  type = string
+}
+
+variable "gcp_instance_image" {
+  type = string
+}
+
+variable "gcp_existing_network_slo" {
+  description = "existing gcp network name slo"
+  type        = string
+}
+
+variable "gcp_existing_network_sli" {
+  description = "existing gcp network name sli"
+  type        = string
+}
+
+variable "gcp_existing_subnet_network_slo" {
+  description = "existing gcp subnetwork name slo"
+  type        = string
+}
+
+variable "gcp_existing_subnet_network_sli" {
+  description = "existing gcp subnetwork name sli"
+  type        = string
+}
